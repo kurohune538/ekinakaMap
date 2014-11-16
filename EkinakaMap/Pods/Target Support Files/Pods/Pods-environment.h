50907c52c90cf186480e506b2c4309c276159924
Shinnosuke Komiya <kurohune538@gmail.com>
2014-11-17 00:01:44 +0900

    .gitignore removed

---
+++ EkinakaMap/Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// DDPageControl
+#define COCOAPODS_POD_AVAILABLE_DDPageControl
+#define COCOAPODS_VERSION_MAJOR_DDPageControl 0
+#define COCOAPODS_VERSION_MINOR_DDPageControl 1
+#define COCOAPODS_VERSION_PATCH_DDPageControl 0
+
